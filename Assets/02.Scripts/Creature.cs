911f3b2709ce42a1d41fc124af0a89e59fe7969b
종 욱 <whddnr0317@naver.com>
2025-03-11 05:49:42 +0900

    1강 유니티 기초 (GameObject, Component, 상속)

---
+++ Assets/02.Scripts/Creature.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    public int hp;
+    public int damage;
+    //...
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
